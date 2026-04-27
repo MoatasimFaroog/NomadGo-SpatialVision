fc620f5b8a306b7e8c974508f0af8f9945daf1ad
MoatasimFaroog <moatasim.gtc@gmail.com>
2026-04-27 11:12:42 +0300

    Add AppStartup class for app initialization
    Implement screen orientation settings and camera permission request for Android.

---
+++ Assets/Scripts/AppStartup.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class AppStartup : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.autorotateToLandscapeLeft = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.autorotateToPortrait = true;
+        Screen.autorotateToPortraitUpsideDown = false;
+    }
+
+    void Start()
+    {
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+#endif
+    }
+}
